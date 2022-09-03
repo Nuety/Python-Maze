1de62ad057063bb4c7fd02df60583725881c3e96
Asger Fihl Hedegaard Nielsen <asger@fihl.net>
2022-09-03 11:31:19 +0200

    Clean up

--- Core/visualiser.py
+++ Core/visualiser.py
@@ -17,8 +17,6 @@ pygame.init()
 win = pygame.display.set_mode((win_width, win_height))
 pygame.display.set_caption('MazeyMan')
 
-clock = pygame.time.Clock()
-
 def cbvisualiser(x, y):
     pygame.draw.rect(win, (150, 102, 51), pygame.Rect(x * cellwidth, y * cellheight, cellwidth + 1, cellheight + 1))
     pygame.display.update()
@@ -31,13 +29,10 @@ maze = generator.newMaze(int(xCells), int(yCells), cbvisualiser)
 
 run = True
 
-
-
 for i in range(int(((xCells * 2) + 1))):
     for j in range(int(((yCells * 2) + 1))):
         if maze[j][i] == True:
             pygame.draw.rect(win, (70, 50, 30), pygame.Rect(i * cellwidth, j * cellheight, cellwidth + 1, cellheight + 1))
-
 
 while run:
     pygame.display.update()
